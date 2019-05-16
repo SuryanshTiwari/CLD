d542d73e58a33816e695859e01580135a1b57a7c
SuryanshTiwari <sandeep.challa@ggktech.com>
2019-05-16 14:16:39 +0530

    added swagger

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 var indexRouter = require('./routes/index');
 var wodRouter = require('./routes/wordOfTheDay');
@@ -25,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', indexRouter);
 app.use('/word-of-the-day', wodRouter);                               // this corresponds to word of the day 
